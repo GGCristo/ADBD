e3b2499dc7d7dd0e6a1d118f26d9859ad7b7137d
GGCristo <cgg.code@gmail.com>
2021-11-16 19:07:08 +0000

    Update trigger

--- trigger/trigger.sql
+++ trigger/trigger.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE FUNCTION crear_email() RETURNS trigger AS $$
       New.email := NEW.nombre || NEW.apellido1 || NEW.apellido2 || '@' || TG_ARGV[0];
       return NEW;
     END IF;
-    IF NEW.email ~= '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$' THEN
+    IF NEW.email != '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$' THEN
       RAISE EXCEPTION 'This email is not correct: %', NEW.email;
     END IF;
   END;
